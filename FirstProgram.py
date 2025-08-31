5ce64289a2ae7ca487d4319cd0a0ba04ee4adfe4
afalkner740 <afalknerpersonal@gmail.com>
2025-08-31 01:02:49 -0500

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,16 +6,16 @@
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
-
+  print("What is your name?")
   #Use the user's name in the program.
-
+  
   #Ask the user for their age.
-
+  print("What is your age?")
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
-
+  
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
