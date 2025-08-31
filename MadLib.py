fd2834197ed6e7484f5dfc70ddfd874a6e6745f1
afalkner740 <afalknerpersonal@gmail.com>
2025-08-31 01:13:46 -0500

    Update MadLib.py

--- MadLib.py
+++ MadLib.py
@@ -1,12 +1,12 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Alexa Falkner
+#Date: 8/31/2025
+#Assignment: MadLib
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  print("What are some words?")
   #Print the story with the user supplied words.
 
 
